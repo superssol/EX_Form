430a7b23492261ac697135bea7f6360e848e262f
superssol <hansole10@gmail.com>
2022-11-08 18:00:49 +0900

    first commit

---
+++ EX_Form/src/main/java/com/exerciese/spring/ExFormApplication.java
@@ -0,0 +1,13 @@
+package com.exerciese.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExFormApplication.class, args);
+	}
+
+}
